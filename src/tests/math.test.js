1528b1634f9cf1474c5ab070c78b393c7e616feb
Mor Guetta <morgueta36@gmail.com>
2025-11-21 10:59:55 +0200

    test with CI.yml working all correct

--- src/tests/math.test.js
+++ src/tests/math.test.js
@@ -1,4 +1,4 @@
 const {sum,sub,mod}=require("../math")
 test("sum 2+3=5",()=> expect(sum(2,3)).toBe(5));
-test("sub 3-2=1",()=> expect(sub(3,2)).toBe(2));
+test("sub 3-2=1",()=> expect(sub(3,2)).toBe(1));
 test("mod 6%2==0",()=> expect(mod(6)).toBe(true));
